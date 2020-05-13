0200a3ea14b51b988f451b143969fe770786d7ba
KevinLin <chengte99@gmail.com>
2020-05-13 10:41:26 +0800

    Java and Kotlin amend

--- src/main/java/com/chengte99/Hello.java
+++ src/main/java/com/chengte99/Hello.java
@@ -7,7 +7,15 @@ public class Hello {
         Person p = new Person();
         p.hello();
 
-        Human h = new Human();
-        h.hello();
+        int age = 19;
+        Integer age2 = 23;
+        char c = 'A';
+        Character c2 = 'B';
+        float weight = 66.5f;
+        Float height = 123.3f;
+        byte b = 123;
+        boolean isEnable = true;
+        Boolean isOK = isEnable;
+        String name = "kevin";
     }
 }
--- src/main/kotlin/com/chengte99/Hello.kt
+++ src/main/kotlin/com/chengte99/Hello.kt
@@ -3,10 +3,14 @@ package com.chengte99
 fun main() {
 //    println("Hello kotlin")
 //    Person().hello()
-    val p = Person()
-    p.hello()
-
     Human().hello()
+
+    var age = 12
+    age = 23
+    var weight: Float = 55.6f
+
+    var name: String
+    name = "Kevin"
 }
 
 class Human {
